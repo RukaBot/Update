7f7dfce166c3f0cb7942e4fd7e766717b14b84cb
RukaBot <74529648+RukaBot@users.noreply.github.com>
2020-11-17 13:43:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'DevaBot'; 
-const instagram = 'https://instagram.com/deva_naufal'; 
-const telegram = '-'; 
+const BotName = 'RUKA BOT'; 
+const instagram = 'https://instagram.com/sndrzqy_'; 
+const telegram = 'ikiiii2906'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
 const youtube = '-';
 //const grupch1 = 'belum ada grup'; 
@@ -64,11 +64,11 @@ fs.existsSync('./session.json') && conn.loadAuthInfo('./session.json')
 //conn.connectOptions.agent = ProxyAgent ('http://1.0.180.120:8080')
 conn.connect();
 
-conn.on('user-presence-update', json => console.log(json.id + ' presence is => ' + json.type)) || console.log('Bot by ig:@reuz.id')
+conn.on('user-presence-update', json => console.log(json.id + ' presence is => ' + json.type)) || console.log('Bot by ig:@sndrzqy_')
 conn.on('message-status-update', json =>
 {
    const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
-   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by ig:@reuz.id`)
+   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by ig:@sndrzqy_`)
 })
 
 conn.on('message-new', async(m) =>
